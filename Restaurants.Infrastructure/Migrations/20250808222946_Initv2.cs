d1405dc69b50fc42aaf02cad58c114e50f7b87fa
Luis Cuya <luis.cuya@intelica.com>
2025-08-08 17:55:34 -0500

    feat: Se agrega semilla data

---
+++ Restaurants.Infrastructure/Migrations/20250808222946_Initv2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurants.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initv2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Restaurants",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "KiloCalories",
+                table: "Dishes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KiloCalories",
+                table: "Dishes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Restaurants",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -0,0 +1,56 @@
+
+using Restaurants.Domain.Entities;
+using Restaurants.Infrastructure.Persistence;
+
+namespace Restaurants.Infrastructure.Seeders;
+
+internal class RestaurantSeeder(RestaurantsDbContext context) : IRestaurantSeeder
+{
+    public async Task Seed()
+    {
+        if (await context.Database.CanConnectAsync())
+        {
+            if (!context.Restaurants.Any())
+            {
+                var restaurants = GetRestaurants();
+                context.Restaurants.AddRange(restaurants);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+
+    private IEnumerable<Restaurant> GetRestaurants()
+    {
+        List<Restaurant> restaurants = [
+            new()
+            {
+                Name = "KFC",
+                Category = "Fast Food",
+                Description = "Fried chicken and fast food, is an American fast food restaurant chain headquartered in Louisville, Kentucky, that specializes in fried chicken.",
+                ContactEmail = "contact@kfc.com",
+                HasDelivery = true,
+                Dishes = [
+                    new()
+                    {
+                        Name = "Chicken Nuggets",
+                        Description = "Chicken nuggets are pieces of chicken that have been breaded or battered, then deep fried or baked.",
+                        Price = 5.99M
+                    },
+                    new()
+                    {
+                        Name = "Chicken Sandwich",
+                        Description = "A chicken sandwich is a sandwich that typically consists of boneless, skinless chicken breast, or fried chicken cutlet.",
+                        Price = 6.99M
+                    }
+                ],
+                Address = new()
+                {
+                    City = "New York",
+                    Street = "5th Avenue",
+                    PostalCode = "10001"
+                }
+            }
+        ];
+        return restaurants;
+    }
+}
