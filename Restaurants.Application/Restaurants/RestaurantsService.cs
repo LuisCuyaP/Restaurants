e43f71503be167d9932dc819935e24065a9c869e
Luis Cuya <luis.cuya@intelica.com>
2025-08-11 11:35:47 -0500

    feat: Se agrega automapper

--- Restaurants.Application/Restaurants/RestaurantsService.cs
+++ Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -1,17 +1,17 @@
+using AutoMapper;
 using Microsoft.Extensions.Logging;
 using Restaurants.Application.Restaurants.Dtos;
-using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Restaurants;
 
-internal class RestaurantsService(IRestaurantsRepository restaurantsRepository, ILogger<RestaurantsService> logger) : IRestaurantsService
+internal class RestaurantsService(IRestaurantsRepository restaurantsRepository, ILogger<RestaurantsService> logger, IMapper mapper) : IRestaurantsService
 {
     public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants()
     {
         logger.LogInformation("Getting all restaurants");
         var restaurants = await restaurantsRepository.GetAllAsync();
-        var restaurantDtos = restaurants.Select(RestaurantDto.FromEntity);
+        var restaurantDtos = mapper.Map<IEnumerable<RestaurantDto>>(restaurants);
         return restaurantDtos!;
     }
 
@@ -19,7 +19,7 @@ internal class RestaurantsService(IRestaurantsRepository restaurantsRepository, 
     {
         logger.LogInformation("Getting restaurant by id: {Id}", id);
         var restaurant = await restaurantsRepository.GetByIdAsync(id);
-        var restaurantDto = RestaurantDto.FromEntity(restaurant);
+        var restaurantDto = mapper.Map<RestaurantDto?>(restaurant);
         return restaurantDto;
     }
 }
