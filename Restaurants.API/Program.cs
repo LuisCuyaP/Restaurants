f3a903c538c86971b3c35f32803b730249dfdefd
Luis Cuya <luis.cuya@intelica.com>
2025-08-13 14:50:34 -0500

    feat: Se agrega serilog

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -2,6 +2,8 @@
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Restaurants.Application.Extensions;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Host.UseSerilog((context, configuration) =>
+    configuration
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
+        .WriteTo.Console(outputTemplate: "{Timestamp:dd-MM HH:mm:ss} [{Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}")
+);
 
 var app = builder.Build();
 
@@ -17,6 +25,7 @@ var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
 await seeder.Seed();
 
+app.UseSerilogRequestLogging();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
