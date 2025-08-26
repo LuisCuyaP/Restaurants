d96a53608d957b9a636d8a188b5c8c82eef34563
Luis Cuya <luis.cuya@intelica.com>
2025-08-26 17:07:43 -0500

    refactor: mejor header post, obtener endpoint del plato creado con el newId del plato generado

--- Restaurants.Application/Dishes/Commands/CreateDish/CreateDishCommandHandler.cs
+++ Restaurants.Application/Dishes/Commands/CreateDish/CreateDishCommandHandler.cs
@@ -7,15 +7,15 @@ using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Dishes.Commands.CreateDish;
 
-public class CreateDishCommandHandler(ILogger<CreateDishCommandHandler> logger, IRestaurantsRepository restaurantsRepository, IDishesRepository dishesRepository, IMapper mapper) : IRequestHandler<CreateDishCommand>
+public class CreateDishCommandHandler(ILogger<CreateDishCommandHandler> logger, IRestaurantsRepository restaurantsRepository, IDishesRepository dishesRepository, IMapper mapper) : IRequestHandler<CreateDishCommand, int>
 {
-    public async Task Handle(CreateDishCommand request, CancellationToken cancellationToken)
+    public async Task<int> Handle(CreateDishCommand request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Handling CreateDishCommand for dish: {@DishRequest}", request);
         var restaurant = await restaurantsRepository.GetByIdAsync(request.RestaurantId);
         if (restaurant == null) throw new NotFoundException(nameof(Restaurant), request.RestaurantId.ToString());
 
         var dish = mapper.Map<Dish>(request);
-        await dishesRepository.Create(dish);
+        return await dishesRepository.Create(dish);
     }
 }
